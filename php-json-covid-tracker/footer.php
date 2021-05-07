fa5f75a526bb633f24638f402185d03e6930240f
maplesyrubweb <73809301+maplesyrupweb@users.noreply.github.com>
2021-05-06 19:00:42 -0700

    Create footer.php

---
+++ php-json-covid-tracker/footer.php
@@ -0,0 +1,22 @@
+<footer class="footer mt-auto py-3 bg-dark">
+      <div class="container text-center">
+        <a href="index.php"><button type="button" class="btn btn-primary" id="myBtn">Home</button></a>
+        <p>
+        <span class="text-muted">Stay Safe and be kind to one another</span>
+      </p>
+      </div>
+</footer>
+
+
+<!-- Bootstrap Jquery  -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+
+<!-- Bootstrap JS  -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap" rel="stylesheet">
+
+<!-- Font Awesome -->
+<script src="https://kit.fontawesome.com/cdffdb631a.js" crossorigin="anonymous"></script>

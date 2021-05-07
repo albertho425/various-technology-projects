41578a81a023b4e16f35c62a94bfd62659828a36
maplesyrubweb <73809301+maplesyrupweb@users.noreply.github.com>
2021-05-06 19:01:21 -0700

    Create header.php

---
+++ php-json-covid-tracker/header.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-xl navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php">Covid 19 Live Tracker</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+
+    </ul>
+  </div>
+</nav>
+
+<!-- Bootstrap CSS  -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
